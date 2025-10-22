79e716ed7aeb8b0a1c682c5557afdf2ffd3dfe52
Tirzaan Neil <Tirzaan@tntneil.com>
2025-10-22 15:33:16 -0600

    Initial Commit

---
+++ SwiftfulSwiftUIInPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftfulSwiftUIInPractice
+//
+//  Created by Tirzaan on 10/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftfulSwiftUIInPractice/SwiftfulSwiftUIInPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSwiftUIInPracticeApp.swift
+//  SwiftfulSwiftUIInPractice
+//
+//  Created by Tirzaan on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSwiftUIInPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
